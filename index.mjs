3efb9ac8182d319c2b55cb8a66a5130f7da2d42b
Kipngetich33 <khalifngeno@gmail.com>
2022-07-10 21:15:31 +0300

    add functionality to inform partipants that the backend is running

--- index.mjs
+++ index.mjs
@@ -6,12 +6,27 @@ import * as backend from './build/index.main.mjs';
 import {ask} from '@reach-sh/stdlib';
 //get the set environment variable for example Algo the default/fallback is Algo
 const stdlib = loadStdlib(process.env);
+
+//create helper functions below
+const getBalance = async (who) => await stdlib.balanceOf(who);
+
+
 //create a parcipant interact
-const participantInteract = {};
+const participantInteract = {
+    startingBackend: () => {
+        console.log("Contract Backend Running")
+    },
+};
+
 
 /* create a new test account and initialiaze the amout to 1000000000 ==
 1000 network tokens on algorand */
 const acc = await stdlib.newTestAccount(1000000000);
+//now show the balance of the account
+getBalance(acc).then((bal) => {
+    console.log(`bal ${bal}`)
+})
+
 const ctc = acc.contract(backend);
 await ctc.participants.Participant(participantInteract);
 
--- index.rsh
+++ index.rsh
@@ -2,12 +2,21 @@
 'reach 0.1';
 
 //create a  Participant interface
-const participantInteract = {};
+const participantInteract = {
+    startingBackend: Fun([], Null),
+};
 
 //export the main reach application 
 export const main = Reach.App(() => {
+
+    //create contract participant
     const P = Participant('Participant', participantInteract);
+
+    //initialize contract
     init();
+
+    //inform participants that the contract backend is running
+    each([P,],() => { interact.startingBackend() });
 
     //just create a publication to avoid publication warning
     P.only(() => {});
